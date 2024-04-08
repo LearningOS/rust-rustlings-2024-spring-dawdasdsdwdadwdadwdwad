c79a6b013bdb0eadff149922f498963a33351244
John Richrdo <ricardos1132001@gmail.com>
2024-04-08 19:20:41 +0800

    update

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -6,8 +6,11 @@
 // I AM NOT DONE
 
 fn main() {
-    let number = "T-H-R-E-E"; // don't change this line
-    println!("Spell a Number : {}", number);
-    number = 3; // don't rename this variable
-    println!("Number plus two is : {}", number + 2);
+    let mut number = "T-H-R-E-E"; // don't change this line
+    println!("Spell a Number : {:?}", number);
+    number = "3"; // don't rename this variable
+    println!(
+        "Number plus two is : {}",
+        number.parse::<i32>().unwrap() + 2
+    );
 }
